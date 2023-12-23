7c4ecdb6af5325057528f0cb44930e35c56f90a5
mnoor920 <mnoor92081@hotmail.com>
2023-12-23 17:44:28 +0500

    React Project Setup

---
+++ src/pages/homePage/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomePage = () => {
+
+
+    return (
+        <div>HomePage
+
+            <h3>
+                React JS & Node Js Batch 803
+            </h3>
+        </div>
+    )
+
+
+}
+
+export default HomePage
